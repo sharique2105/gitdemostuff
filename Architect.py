b4bf2c7bd76c2b57b9b77a8184911cf921a3e9da
sharique <shariqueshaikh1@yahoo.com>
2024-09-16 18:41:15 -0400

    EST Architect changes

--- Architect.py
+++ Architect.py
@@ -4,6 +4,10 @@ print("Architect framework change 6")
 print("Framework change by architect 7")
 print("Few more architect changes 8")
 
+print("EST Architect changes 9")
+print("EST Architect changes 10")
+print("EST Architect changes 11")
+
 
 
 
